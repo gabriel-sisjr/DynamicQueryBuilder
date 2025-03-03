56c17b02f1af31b587e8ae4c942744d550c699cf
Gabriel Santana <36143255+gabriel-sisjr@users.noreply.github.com>
2025-03-03 15:03:45 +0000

    Settings to Documentation Generation (#2)
    * feat: adding docfx to generate project documentation
    
    * chore: excluding node_modules from doc generation
    
    * chore: creating yml to manage github page documentation
    
    * chore: update docfx.yml to allow build on .NET 8 and 9
    
    * chore: updating version of actions from v3 to v4

--- src/DynamicQueryBuilder/Class1.cs
+++ src/DynamicQueryBuilder/Class1.cs
@@ -1,5 +1,14 @@
 ﻿namespace DynamicQueryBuilder;
 
+/// <summary>
+/// Class Summary Test
+/// </summary>
 public class Class1
 {
+    /// <summary>
+    /// Method to Sum all items from params.
+    /// </summary>
+    /// <param name="values">Items to be summ.</param>
+    /// <returns>The total value of summ.</returns>
+    public int SumItems(params int[] values) => values.Sum();
 }
