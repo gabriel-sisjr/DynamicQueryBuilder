230985e00584aed8f53ad5a08b25706fe5fc7c87
Gabriel Santana <gabriel.sistemasjr@gmail.com>
2025-03-04 13:56:42 +0000

    chore: adding WebAPI project to use as example

---
+++ examples/ClientWebAPI/ClientWebAPI/Program.cs
@@ -0,0 +1,28 @@
+using DynamicQueryBuilder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDynamicQueryBuilder(builder.Configuration.GetConnectionString("DynamicQueryBuilder")
+    ?? throw new InvalidOperationException("Connection string not found."));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
